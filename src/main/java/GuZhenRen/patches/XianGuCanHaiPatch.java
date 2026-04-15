5dc5c2df3dbbdd15b91f8b4b36737f0b49a56540
PetStzx <2454771984@qq.com>
2026-04-16 02:02:35 +0800

    修复了触发“仙蛊残骸”导致无法离开篝火的问题

--- src/main/java/GuZhenRen/patches/XianGuCanHaiPatch.java
+++ src/main/java/GuZhenRen/patches/XianGuCanHaiPatch.java
@@ -1,25 +1,32 @@
 package GuZhenRen.patches;
 
 import GuZhenRen.relics.XianGuCanHai;
+import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.CampfireUI;
 import com.megacrit.cardcrawl.rooms.RestRoom;
+import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
+import com.megacrit.cardcrawl.ui.campfire.DigOption;
+import com.megacrit.cardcrawl.ui.campfire.LiftOption;
+import com.megacrit.cardcrawl.ui.campfire.RestOption;
+import com.megacrit.cardcrawl.ui.campfire.SmithOption;
+import com.megacrit.cardcrawl.ui.campfire.TokeOption;
 import com.megacrit.cardcrawl.vfx.campfire.CampfireSmithEffect;
+
+import java.util.ArrayList;
 
 @SpirePatch(clz = CampfireSmithEffect.class, method = "update")
 public class XianGuCanHaiPatch {
-    // 标记1：判断是否是本地玩家点击的（防联机错乱）
     public static boolean isLocalAction = false;
-
-    // 标记2：判断玩家是否真的选中了卡牌
     public static boolean cardWasSelected = false;
 
     @SpirePrefixPatch
     public static void Prefix(CampfireSmithEffect __instance) {
-        // 在原版 update 逻辑执行之前，检查选牌列表是否非空
         if (AbstractDungeon.gridSelectScreen != null && !AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
             cardWasSelected = true;
         }
@@ -28,20 +35,56 @@ public class XianGuCanHaiPatch {
     @SpirePostfixPatch
     public static void Postfix(CampfireSmithEffect __instance) {
         if (__instance.isDone) {
-            // 同时满足 本地点击 且 真的选了牌，才消耗遗物层数
             if (isLocalAction && cardWasSelected && AbstractDungeon.player.hasRelic(XianGuCanHai.ID)) {
                 XianGuCanHai relic = (XianGuCanHai) AbstractDungeon.player.getRelic(XianGuCanHai.ID);
 
                 if (relic.counter > 0) {
-                    relic.useCharge();
-                    AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.INCOMPLETE;
+
+                    boolean hasUpgradable = false;
+                    for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+                        if (c.canUpgrade()) {
+                            hasUpgradable = true;
+                            break;
+                        }
+                    }
+
+                    boolean hasActionEndingOptions = false;
                     if (AbstractDungeon.getCurrRoom() instanceof RestRoom) {
-                        ((RestRoom) AbstractDungeon.getCurrRoom()).campfireUI.reopen();
+                        RestRoom room = (RestRoom) AbstractDungeon.getCurrRoom();
+
+                        @SuppressWarnings("unchecked")
+                        ArrayList<AbstractCampfireOption> buttons = (ArrayList<AbstractCampfireOption>) ReflectionHacks.getPrivate(
+                                room.campfireUI,
+                                CampfireUI.class,
+                                "buttons"
+                        );
+
+                        for (AbstractCampfireOption option : buttons) {
+                            if (option instanceof SmithOption) {
+                                option.usable = hasUpgradable;
+                            }
+
+                            // 白名单
+                            if (option.usable) {
+                                if (option instanceof RestOption ||
+                                        option instanceof SmithOption ||
+                                        option instanceof TokeOption ||
+                                        option instanceof DigOption ||
+                                        option instanceof LiftOption) {
+
+                                    hasActionEndingOptions = true;
+                                }
+                            }
+                        }
+
+                        if (hasActionEndingOptions) {
+                            relic.useCharge();
+                            AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.INCOMPLETE;
+                            room.campfireUI.reopen();
+                        }
                     }
                 }
             }
-
-            // 动作结束后，关锁
             isLocalAction = false;
             cardWasSelected = false;
         }
