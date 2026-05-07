bc4f75de49b9765620939766f1f2d4e4c709524d
PetStzx <2454771984@qq.com>
2026-05-07 20:07:07 +0800

    新增地灾“浪静”及相应power

---
+++ src/main/java/GuZhenRen/powers/LangJingPower.java
@@ -0,0 +1,76 @@
+package GuZhenRen.powers;
+
+import GuZhenRen.GuZhenRen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class LangJingPower extends AbstractPower {
+    public static final String POWER_ID = GuZhenRen.makeID("LangJingPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public LangJingPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        String pathLarge = GuZhenRen.assetPath("img/powers/LangJingPower_p.png");
+        String pathSmall = GuZhenRen.assetPath("img/powers/LangJingPower.png");
+        Texture texLarge = ImageMaster.loadImage(pathLarge);
+        Texture texSmall = ImageMaster.loadImage(pathSmall);
+        this.region128 = new TextureAtlas.AtlasRegion(texLarge, 0, 0, 88, 88);
+        this.region48 = new TextureAtlas.AtlasRegion(texSmall, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        if (this.amount > 0) {
+            this.flash();
+        }
+        this.amount = 0;
+        this.updateDescription();
+    }
+
+    @Override
+    public void wasHPLost(DamageInfo info, int damageAmount) {
+        if (info != null && info.type != DamageInfo.DamageType.HP_LOSS && damageAmount > 0) {
+            this.flash();
+            this.amount += 1;
+            this.updateDescription();
+        }
+    }
+
+    @Override
+    public float atDamageReceive(float damage, DamageInfo.DamageType type) {
+        if (damage > 0 && type == DamageInfo.DamageType.NORMAL) {
+            return Math.max(0.0F, damage - this.amount);
+        }
+        return damage;
+    }
+
+    @Override
+    public int onAttackedToChangeDamage(DamageInfo info, int damageAmount) {
+        if (damageAmount > 0 && info != null && info.type != DamageInfo.DamageType.NORMAL && info.type != DamageInfo.DamageType.HP_LOSS) {
+            return Math.max(0, damageAmount - this.amount);
+        }
+        return damageAmount;
+    }
+}
