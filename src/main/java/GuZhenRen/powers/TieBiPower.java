f655eb18c37053a0b62be277913984371e78333a
PetStzx <2454771984@qq.com>
2026-05-07 20:10:23 +0800

    新增天劫“铁壁”及相应power

---
+++ src/main/java/GuZhenRen/powers/TieBiPower.java
@@ -0,0 +1,49 @@
+package GuZhenRen.powers;
+
+import GuZhenRen.GuZhenRen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class TieBiPower extends AbstractPower {
+    public static final String POWER_ID = GuZhenRen.makeID("TieBiPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public TieBiPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        String pathLarge = GuZhenRen.assetPath("img/powers/TieBiPower_p.png");
+        String pathSmall = GuZhenRen.assetPath("img/powers/TieBiPower.png");
+        Texture texLarge = ImageMaster.loadImage(pathLarge);
+        Texture texSmall = ImageMaster.loadImage(pathSmall);
+        this.region128 = new TextureAtlas.AtlasRegion(texLarge, 0, 0, 88, 88);
+        this.region48 = new TextureAtlas.AtlasRegion(texSmall, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = String.format(DESCRIPTIONS[0], this.amount);
+    }
+
+
+    @Override
+    public void atStartOfTurn() {
+        this.flash();
+        this.addToBot(new GainBlockAction(this.owner, this.owner, this.amount));
+    }
+}
