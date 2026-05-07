657e7608b8bcaac146f21d7ba0eee0a912e51c42
PetStzx <2454771984@qq.com>
2026-05-07 20:17:31 +0800

    新增浩劫“松针”及相应power

---
+++ src/main/java/GuZhenRen/powers/SongZhenPower.java
@@ -0,0 +1,61 @@
+package GuZhenRen.powers;
+
+import GuZhenRen.GuZhenRen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.ThornsPower;
+
+public class SongZhenPower extends AbstractPower {
+    public static final String POWER_ID = GuZhenRen.makeID("SongZhenPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public SongZhenPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        String pathLarge = GuZhenRen.assetPath("img/powers/SongZhenPower_p.png");
+        String pathSmall = GuZhenRen.assetPath("img/powers/SongZhenPower.png");
+        Texture texLarge = ImageMaster.loadImage(pathLarge);
+        Texture texSmall = ImageMaster.loadImage(pathSmall);
+        this.region128 = new TextureAtlas.AtlasRegion(texLarge, 0, 0, 88, 88);
+        this.region48 = new TextureAtlas.AtlasRegion(texSmall, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        String coloredName = this.owner.name.replace(" ", " #y");
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1] + coloredName + DESCRIPTIONS[2];
+    }
+
+    @Override
+    public void duringTurn() {
+        this.flash();
+        this.addToBot(new ApplyPowerAction(this.owner, this.owner, new ThornsPower(this.owner, this.amount), this.amount));
+    }
+
+    @Override
+    public float atDamageGive(float damage, DamageInfo.DamageType type) {
+        if (type == DamageInfo.DamageType.NORMAL) {
+            if (this.owner.hasPower(ThornsPower.POWER_ID)) {
+                damage += this.owner.getPower(ThornsPower.POWER_ID).amount;
+            }
+        }
+        return damage;
+    }
+}
