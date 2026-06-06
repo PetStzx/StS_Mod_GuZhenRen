f31af951487dab6a1e381c5f8804c79481461ac1
PetStzx <2454771984@qq.com>
2026-06-06 19:32:06 +0800

    调整“火冒三丈蛊”

--- src/main/java/GuZhenRen/cards/HuoMaoSanZhangGu.java
+++ src/main/java/GuZhenRen/cards/HuoMaoSanZhangGu.java
@@ -2,10 +2,12 @@ package GuZhenRen.cards;
 
 import GuZhenRen.GuZhenRen;
 import GuZhenRen.patches.CardColorEnum;
+import GuZhenRen.powers.FenShaoPower;
 import GuZhenRen.powers.HuoMaoSanZhangPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -16,8 +18,7 @@ public class HuoMaoSanZhangGu extends AbstractGuZhenRenCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String IMG_PATH = GuZhenRen.assetPath("img/cards/HuoMaoSanZhangGu.png");
 
-    private static final int COST = 3;
-    private static final int UPGRADED_COST = 2; // 升级 2 费
+    private static final int COST = 2;
     private static final int INITIAL_RANK = 4;
 
     public HuoMaoSanZhangGu() {
@@ -29,12 +30,22 @@ public class HuoMaoSanZhangGu extends AbstractGuZhenRenCard {
 
         this.setDao(Dao.YAN_DAO);
         this.setRank(INITIAL_RANK);
+
+        this.baseFenShao = this.fenShao = 3;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (this.upgraded) {
+            for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                if (!mo.isDeadOrEscaped()) {
+                    this.addToBot(new ApplyPowerAction(mo, p, new FenShaoPower(mo, this.fenShao), this.fenShao));
+                }
+            }
+        }
+
         if (!p.hasPower(HuoMaoSanZhangPower.POWER_ID)) {
-            this.addToBot(new ApplyPowerAction(p, p, new HuoMaoSanZhangPower(p,-1)));
+            this.addToBot(new ApplyPowerAction(p, p, new HuoMaoSanZhangPower(p, -1)));
         }
     }
 
@@ -42,7 +53,7 @@ public class HuoMaoSanZhangGu extends AbstractGuZhenRenCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeBaseCost(UPGRADED_COST);
+            this.myBaseDescription = cardStrings.UPGRADE_DESCRIPTION;
             this.upgradeRank(1);
             this.initializeDescription();
         }
