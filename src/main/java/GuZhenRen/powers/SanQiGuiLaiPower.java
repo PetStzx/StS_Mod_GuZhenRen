7a6b41d22194e435f6ef25ca6b08e7ccf78ea05f
PetStzx <2454771984@qq.com>
2026-08-09 15:04:25 +0800

    新增power“三气归来”

---
+++ src/main/java/GuZhenRen/powers/SanQiGuiLaiPower.java
@@ -0,0 +1,50 @@
+package GuZhenRen.powers;
+
+import GuZhenRen.GuZhenRen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class SanQiGuiLaiPower extends AbstractPower {
+    public static final String POWER_ID = GuZhenRen.makeID("SanQiGuiLaiPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public SanQiGuiLaiPower(AbstractCreature owner) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = -1;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = true;
+
+        String pathLarge = GuZhenRen.assetPath("img/powers/SanQiGuiLaiPower_p.png");
+        String pathSmall = GuZhenRen.assetPath("img/powers/SanQiGuiLaiPower.png");
+
+        Texture largeTexture = ImageMaster.loadImage(pathLarge);
+        Texture smallTexture = ImageMaster.loadImage(pathSmall);
+        this.region128 = new TextureAtlas.AtlasRegion(largeTexture, 0, 0, 88, 88);
+        this.region48 = new TextureAtlas.AtlasRegion(smallTexture, 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    @Override
+    public void atEndOfRound() {
+        this.flash();
+        this.addToBot(new HealAction(this.owner, this.owner, this.owner.maxHealth));
+        this.addToBot(new ApplyPowerAction(this.owner, this.owner, new JiuLongWenHuShenPower(this.owner, 9), 9));
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+}
