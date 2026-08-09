94cbd9e003b2d34f572fb4bde33d492d105a688c
PetStzx <2454771984@qq.com>
2026-08-09 17:40:20 +0800

    新增power“专注”

---
+++ src/main/java/GuZhenRen/powers/ZhuanZhuPower.java
@@ -0,0 +1,40 @@
+package GuZhenRen.powers;
+
+import GuZhenRen.GuZhenRen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class ZhuanZhuPower extends AbstractPower {
+    public static final String POWER_ID = GuZhenRen.makeID("ZhuanZhuPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public ZhuanZhuPower(AbstractCreature owner) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = -1;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        String pathLarge = GuZhenRen.assetPath("img/powers/ZhuanZhuPower_p.png");
+        String pathSmall = GuZhenRen.assetPath("img/powers/ZhuanZhuPower.png");
+        Texture texLarge = ImageMaster.loadImage(pathLarge);
+        Texture texSmall = ImageMaster.loadImage(pathSmall);
+        this.region128 = new TextureAtlas.AtlasRegion(texLarge, 0, 0, 88, 88);
+        this.region48 = new TextureAtlas.AtlasRegion(texSmall, 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+}
