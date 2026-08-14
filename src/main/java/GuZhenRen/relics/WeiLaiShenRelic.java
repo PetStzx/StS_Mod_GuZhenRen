08d840e2db9bc4143e855a824909056c4cec79a5
PetStzx <2454771984@qq.com>
2026-08-15 00:45:07 +0800

    为“未来身”遗物添加可用时的闪烁提示

--- src/main/java/GuZhenRen/relics/WeiLaiShenRelic.java
+++ src/main/java/GuZhenRen/relics/WeiLaiShenRelic.java
@@ -3,6 +3,7 @@ package GuZhenRen.relics;
 import GuZhenRen.GuZhenRen;
 import GuZhenRen.cards.WeiLaiShen;
 import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.mod.stslib.relics.ClickableRelic;
@@ -13,6 +14,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToHandEffect;
@@ -25,11 +27,21 @@ public class WeiLaiShenRelic extends CustomRelic implements ClickableRelic {
 
     private static final int MAX_BATTLES = 3;
     private boolean isUsedUp = false;
+    private float checkTimer = 0.5f;
+    private boolean isBattleActive = false;
 
     public WeiLaiShenRelic() {
         super(ID, ImageMaster.loadImage(IMG), new Texture(OUTLINE), RelicTier.SPECIAL, LandingSound.MAGICAL);
         this.counter = MAX_BATTLES;
         this.updateDescriptionDynamically();
+    }
+
+    private boolean isSafeInCombat() {
+        return AbstractDungeon.player != null &&
+                AbstractDungeon.isPlayerInDungeon() &&
+                AbstractDungeon.currMapNode != null &&
+                AbstractDungeon.getCurrRoom() != null &&
+                AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
     }
 
     @Override
@@ -49,23 +61,62 @@ public class WeiLaiShenRelic extends CustomRelic implements ClickableRelic {
     public void setCounter(int setCounter) {
         this.counter = setCounter;
         this.updateDescriptionDynamically();
+
+        if (isSafeInCombat()) {
+            this.isBattleActive = true;
+        }
     }
 
     @Override
     public void atBattleStart() {
         if (this.counter > 0) {
+            this.isBattleActive = true;
             this.flash();
             this.addToBot(new ApotheosisAction());
         }
     }
 
     @Override
-    public void onRightClick() {
-        boolean inCombat = AbstractDungeon.isPlayerInDungeon() &&
-                AbstractDungeon.getCurrRoom() != null &&
-                AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
+    public void update() {
+        super.update();
 
-        if (inCombat) {
+        if (AbstractDungeon.player == null || this.isUsedUp) {
+            this.stopPulse();
+            return;
+        }
+
+        if (isSafeInCombat()) {
+            this.checkTimer -= Gdx.graphics.getDeltaTime();
+            if (this.checkTimer <= 0.0f) {
+                this.checkTimer = 0.5f;
+                boolean shouldPulse = false;
+
+                if (!AbstractRecipeRelic.usedWeiLaiShenThisCombat) {
+                    for (AbstractRelic r : AbstractDungeon.player.relics) {
+                        if (r instanceof AbstractRecipeRelic) {
+                            AbstractRecipeRelic recipe = (AbstractRecipeRelic) r;
+                            if (!recipe.usedUp && recipe.canBeBorrowedByWeiLaiShen()) {
+                                shouldPulse = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (shouldPulse) {
+                    this.beginLongPulse();
+                } else {
+                    this.stopPulse();
+                }
+            }
+        } else {
+            this.stopPulse();
+        }
+    }
+
+    @Override
+    public void onRightClick() {
+        if (isSafeInCombat()) {
             if (AbstractDungeon.isScreenUp || AbstractDungeon.actionManager.turnHasEnded || AbstractDungeon.player.isDead) {
                 return;
             }
@@ -78,13 +129,11 @@ public class WeiLaiShenRelic extends CustomRelic implements ClickableRelic {
     private void returnCardAndRemove() {
         if (this.isUsedUp) return;
         this.isUsedUp = true;
+        this.isBattleActive = false;
 
         AbstractPlayer p = AbstractDungeon.player;
-        boolean inCombat = AbstractDungeon.isPlayerInDungeon() &&
-                AbstractDungeon.getCurrRoom() != null &&
-                AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
 
-        if (inCombat) {
+        if (isSafeInCombat()) {
             AbstractDungeon.actionManager.addToBottom(new AbstractGameAction() {
                 @Override
                 public void update() {
@@ -105,12 +154,19 @@ public class WeiLaiShenRelic extends CustomRelic implements ClickableRelic {
         }
     }
 
+    private void handleCombatEnd() {
+        if (this.isBattleActive) {
+            this.isBattleActive = false;
+            if (!this.isUsedUp && this.counter > 0) {
+                this.counter--;
+                this.updateDescriptionDynamically();
+            }
+        }
+    }
+
     @Override
     public void onVictory() {
-        if (!this.isUsedUp && this.counter > 0) {
-            this.counter--;
-            this.updateDescriptionDynamically();
-        }
+        handleCombatEnd();
 
         if ((this.isUsedUp || this.counter <= 0) && AbstractDungeon.player.relics.contains(this)) {
             this.isUsedUp = true;
